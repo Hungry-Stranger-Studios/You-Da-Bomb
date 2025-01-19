72502ac124beb5b6b6ddba53e0dbf4a9a950153c
keeryn04 <keerynjohnson@gmail.com>
2025-01-19 16:48:18 -0700

    (MISC) File movements

--- You Da Bomb/Assets/Managers/GAME MANAGER/GameManager.cs
+++ You Da Bomb/Assets/Managers/GAME MANAGER/GameManager.cs
@@ -6,9 +6,6 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
-    public bool IsPaused { get; private set; }
-    private bool isGameActive;
-
     private void Awake()
     {
         if (Instance == null)
@@ -20,20 +17,5 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-    }
-    private void Start()
-    {
-        isGameActive = false;
-        LoadMenuScene();
-    }
-
-    private void LoadMenuScene()
-    {
-        Debug.Log("Menu displayed");
-    }
-
-    private void LoadMainScene()
-    {
-        Debug.Log("Main displayed");
     }
 }
