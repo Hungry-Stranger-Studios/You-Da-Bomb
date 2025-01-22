cdb4aa86aaeda83c7147d46b765d946be004017b
keeryn04 <keerynjohnson@gmail.com>
2025-01-22 16:19:37 -0700

    (FEATURE) Cursor Sprite

---
+++ You Da Bomb/Assets/UI/Cursor/CursorSprite.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorSprite : MonoBehaviour
+{
+    private Camera mainCamera;
+    private Animator animator;
+
+    [Header("Cursor Follow Settings")]
+    [SerializeField] private float followSmoothness = 10f;
+
+    void Awake()
+    {
+        mainCamera = Camera.main;
+        animator = GetComponent<Animator>();
+
+        //Hide default cursor
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        SmoothFollowCursor();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            PlayClickAnimation();
+        }
+    }
+
+    private void SmoothFollowCursor()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 10f; //Distance from camera
+        Vector3 targetPosition = mainCamera.ScreenToWorldPoint(mousePosition);
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, followSmoothness * Time.deltaTime);
+    }
+
+    private void PlayClickAnimation()
+    {
+        animator.SetTrigger("Click");  
+    }
+
+    public void EndClickAnimation()
+    {
+        animator.ResetTrigger("Click");
+    }
+}
