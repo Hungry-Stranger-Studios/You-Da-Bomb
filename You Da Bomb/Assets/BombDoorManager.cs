7ce962ff9a99a7f60373a802e50c7b0c877433cf
Graham-R7 <Ryan.graham3@ucalgary.ca>
2025-01-30 09:34:56 -0700

    Added all changes except for mainscene

---
+++ You Da Bomb/Assets/BombDoorManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombDoorManager : MonoBehaviour
+{
+    public Vector2Int GridPosition; // The position of the bomb door in the grid
+}
