35ebec246cda67727b1bd18bbdfa18f799f34d08
Graham-R7 <Ryan.graham3@ucalgary.ca>
2025-01-30 21:55:33 -0700

    Important line change for compilation

--- You Da Bomb/Assets/Interactables/Bomb/Eyes/EyeLogic.cs
+++ You Da Bomb/Assets/Interactables/Bomb/Eyes/EyeLogic.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
 using Vector3 = UnityEngine.Vector3;
 
 public class EyeLogic : MonoBehaviour
